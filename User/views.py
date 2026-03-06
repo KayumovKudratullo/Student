97d61e339118fe1bd3ff0ab209ad85c3654ef096
KaymovKudratullo <qayumovqudratullo17@gmail.com>
2026-03-06 23:06:43 +0100

    second update

--- User/views.py
+++ User/views.py
@@ -5,4 +5,9 @@ def home_page(request):
     data = Lenovo.objects.all()
     context = {}
     context['datas'] = data
+    if request.method == 'POST':
+        product = Lenovo.objects.create(
+            title = request.POST['title'],
+            series_number = request.POST['series_number'],
+        )
     return render(request, 'index.html', context)
